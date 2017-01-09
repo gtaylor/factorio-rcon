61bdfe779ea62e98c78dbeca5f3db03dc0482bd5
Greg Taylor <snagglepants@gmail.com>
2017-01-08 21:40:31 -0800

    Add CmdAdmins() RCON command.

--- factorio.go
+++ factorio.go
@@ -40,3 +40,30 @@ func (r *RCON) CmdPlayers() (players []Player, err error) {
 	}
 	return
 }
+
+// CmdAdmins returns all registered admin players on the server.
+func (r *RCON) CmdAdmins() (players []Player, err error) {
+	resp, err := r.Execute("/admins")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(resp.Body, "\n")
+	for _, line := range lines {
+		if len(strings.TrimSpace(line)) == 0 {
+			// Last line is just a return. Do not want.
+			continue
+		}
+
+		var name string
+		var online bool
+		if strings.HasSuffix(line, "(online)") {
+			nameStatusSplit := strings.Split(line, "(")
+			name = strings.TrimSpace(nameStatusSplit[0])
+			online = true
+		} else {
+			name = strings.TrimSpace(line)
+		}
+		players = append(players, Player{Name: name, Online: online})
+	}
+	return
+}
